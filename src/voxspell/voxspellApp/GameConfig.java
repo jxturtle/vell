e9c09bcf951856f2195465eae0996df9f02594aa
jxturtle <jxia616@aucklanduni.ac.nz>
2016-09-05 20:59:20 +1200

    Add files via upload

--- src/voxspell/voxspellApp/GameConfig.java
+++ src/voxspell/voxspellApp/GameConfig.java
@@ -1,15 +1,24 @@
 package voxspell.voxspellApp;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GameConfig {
 	//Name of the file, placed in the same location as the source folder
 	//of the current directory of the project.
 	private static final String FILENAME = "NZCER-spelling-lists.txt";
 	private ArrayList<String> _words;
-	private HashMap<Integer, ArrayList<String>> _wordMap;
+	private ArrayList<String> _wordArray;
+	//private HashMap<Integer, ArrayList<String>> _wordMap;
+	private HashMap<Integer, ArrayList<String>> _finalWordMap;
+	private int _key;
 	
 	private static GameConfig instance;
 	public static GameConfig instance() {
@@ -20,25 +29,50 @@ public class GameConfig {
 	}
 	
 	private GameConfig() {
-		System.out.println(new File(FILENAME).isFile());
-		//Empty implementation. call variable _wordMap
-		//Load all of the words into a dictionary/hashtable with
-		//the keys being the level, and the ArrayList<String> being
-		//the word values for that level. 
-		//Subject to change...
+		try {
+			HashMap<Integer, ArrayList<String>> _wordMap = new HashMap<Integer, ArrayList<String>>();
+			ArrayList<String> _wordArray = new ArrayList<String>();
+			Scanner scanner = new Scanner(new File(FILENAME));
+			while (scanner.hasNextLine()) {
+				
+				String token = scanner.nextLine();
+				//System.out.println(_wordArray);
+				if (token.contains("%Level")) {
+					//System.out.println(_wordMap);
+					if (!_wordArray.isEmpty()) {
+						_key = Integer.parseInt(token.split(" ")[1]);
+						//System.out.println(_key-1 + " " + _wordArray);
+						_wordMap.put(_key-1, _wordArray);
+						//System.out.println(_wordMap);
+						_wordArray = new ArrayList<String>();
+
+					}
+					
+				} else {
+					_wordArray.add(token);
+				}
+				
+			}
+				//System.out.println(_key + " " + _wordArray);
+				_wordMap.put(_key, _wordArray);
+				_finalWordMap = _wordMap;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 	
 	
 	public ArrayList<String> getLevelWords(int level) { 
-		//Empty implementation
-		//Using the _wordMap, we will get the words for our
-		//level, passed in as a parameter to this method.
+		_words = _finalWordMap.get(level);
 		return _words;
 	}
-	
-	public static void main(String[] args) {
-		GameConfig config = GameConfig.instance();
-		
-	}
+//	
+//	public static void main(String[] args) {
+//		GameConfig config = GameConfig.instance();
+//		config.getLevelWords(3);
+//		
+//	}
 }
