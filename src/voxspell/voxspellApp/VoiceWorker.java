a984ee73853c2f574fc88e2e0746f4f369d5e498
jxturtle <jxia616@aucklanduni.ac.nz>
2016-09-13 18:39:51 +1200

    Update VoiceWorker.java

--- src/voxspell/voxspellApp/VoiceWorker.java
+++ src/voxspell/voxspellApp/VoiceWorker.java
@@ -1,29 +1,36 @@
 package voxspell.voxspellApp;
 
-import javax.swing.JTextField;
 import javax.swing.SwingWorker;
+
+import java.io.OutputStream;
 
 public class VoiceWorker extends SwingWorker<Void, Void>{
 	private int _sleep;
 	private String _command;
-	private JTextField _inputField;
+	Process process;
+	OutputStream output;
 	
-	public VoiceWorker(int sleep, String command, JTextField inputField) {
+	public VoiceWorker(int sleep, String command) {
 		_sleep = sleep;
 		_command = command;
-		_inputField = inputField;
 	}
-
+	//output.write("(name_of_voice)n".getBytes());
 	@Override
 	protected Void doInBackground() throws Exception {
 		Thread.sleep(_sleep);
-		ProcessBuilder pb = new ProcessBuilder("bash", "-c", _command);
-		Process process = pb.start();
+		Runtime rt = Runtime.getRuntime();
+		process = rt.exec("festival --pipe");
+		output = process.getOutputStream();
+		output.write(("(SayText \"" + _command + "\" )n").getBytes());
+		output.flush();
 		process.waitFor();
-		process.destroy();
+		//System.out.println("SayText Hello -o | festival --tts");
+		//rt.exec("SayText Hello -o");
+		//rt.exec("SayText" + _command + ""
+//		ProcessBuilder pb = new ProcessBuilder("bash", "-c", _command);
+//		Process process = pb.start();
+//		process.waitFor();
+//		process.destroy();
 		return null;
-	}
-//	protected void done() {
-//		_inputField.setEnabled(true);
-//	}
+	}	
 }
