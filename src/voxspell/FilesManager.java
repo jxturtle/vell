2f2b9e90f3e6d7675d25598def55adb55826fb4b
bchj513 <bcha099@aucklanduni.ac.nz>
2016-09-17 20:48:03 +1200

    Add files via upload

--- src/voxspell/FilesManager.java
+++ src/voxspell/FilesManager.java
@@ -7,36 +7,31 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
 public class FilesManager {
-	public static final String WORDSASSESSED = ".wordsUsed.txt";
-	private String _word;
+	public static final String WORDSASSESSED = ".wordsUsed";
 	private int _token;
-	private String _fileName;
-	private String _statsFileName;
+	private String _word, _fileName, _statsFileName;
 	
 	public FilesManager(String word, int token) {
 		_word = word;
 		_token = token;
 		setUpFileNames();		
-	}
-	
+	}	
 	public void manageFiles() {
 		writeToStatsFile();
 		writeToFile(_fileName);
 		writeToFile(WORDSASSESSED);
 	}
-	
 	private void setUpFileNames() {
 		if (_token == 3) {
-			_fileName = ".mastered.txt";
-			_statsFileName = ".masteredStats.txt";
+			_fileName = ".mastered";
+			_statsFileName = ".masteredStats";
 		} else if (_token == 2) {
-			_fileName = ".failed.txt";
-			_statsFileName = ".failedStats.txt";
+			_fileName = ".failed";
+			_statsFileName = ".failedStats";
 		} else if (_token == 4) {
-			_fileName = ".mastered.txt";
-			_statsFileName = ".faultedStats.txt";
+			_fileName = ".mastered";
+			_statsFileName = ".faultedStats";
 		}
 		//The difference between writeToStatsFile and writeToFile is that writeToFile checks if
 		//the word to be added already exists in the file (Word will be added to the
@@ -50,62 +45,55 @@ public class FilesManager {
 		//correct word from failed and faulted. 
 		if (_token == 3 || _token == 4) {
 			try {
-				File failedFile = new File(".failed.txt");
-				File faultedFile = new File(".faulted.txt");
+				File failedFile = new File(".failed");
+				File faultedFile = new File(".faulted");
 				if (failedFile.exists()) {
 					remakeFileWithoutWord(failedFile);
 				}
 				if (faultedFile.exists()) {
 					remakeFileWithoutWord(faultedFile);
-				}
-			
+				}		
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-		private void remakeFileWithoutWord(File inputFile) throws IOException {
-			File tempFile = new File(".tempWordList.txt");
-			
-			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-			BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-			
-			String currentLine;
-			while ((currentLine = reader.readLine()) != null) {
-				String trimmedLine = currentLine.trim();
-				if (trimmedLine.equalsIgnoreCase(_word)) continue;
-				writer.write(currentLine + "\n");
-			}
-			reader.close();
-			writer.close();
-			inputFile.delete();
-			tempFile.renameTo(inputFile);
-		
-	}
-
-		private void writeToStatsFile() {
-			try {
-				File statsFile = new File(_statsFileName);
-				
-				if (!statsFile.exists()) {
-					statsFile.createNewFile();
-				}
-				
-				BufferedWriter fileWriter = new BufferedWriter(new FileWriter(statsFile.getName(), true));
-				//BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
-				fileWriter.write(_word + "\n");
-				fileWriter.close();
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+	private void remakeFileWithoutWord(File inputFile) throws IOException {
+		File tempFile = new File(".tempWordList");		
+		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+		String currentLine;
+		while ((currentLine = reader.readLine()) != null) {
+			String trimmedLine = currentLine.trim();
+			if (trimmedLine.equalsIgnoreCase(_word)) continue;
+			writer.write(currentLine + "\n");
 		}
-		
-		private void writeToFile(String fileName) {
+		reader.close();
+		writer.close();
+		inputFile.delete();
+		tempFile.renameTo(inputFile);
+	}
+	private void writeToStatsFile() {
 		try {
-			File file = new File(fileName);
-					
+			File statsFile = new File(_statsFileName);
+
+			if (!statsFile.exists()) {
+				statsFile.createNewFile();
+			}
+
+			BufferedWriter fileWriter = new BufferedWriter(new FileWriter(statsFile.getName(), true));
+			//BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+			fileWriter.write(_word + "\n");
+			fileWriter.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private void writeToFile(String fileName) {
+		try {
+			File file = new File(fileName);					
 			if (!file.exists()) {
 				file.createNewFile();
 				BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file.getName(), true));
@@ -118,24 +106,18 @@ public class FilesManager {
 				String wordFromFile;
 				boolean found = false;
 				while ((wordFromFile = br.readLine()) != null) {
-		
 					if (wordFromFile.equals(_word)) {
 						found = true;
-					}
-					
+					}	
 				}
 				if (!found) {
 					BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file.getName(), true));
 					fileWriter.write(_word + "\n");
 					fileWriter.close();
 				}
-				
 			}
 		} catch (IOException e ){
 			e.printStackTrace();
 		}
-		
-
 	}
-	
 }
