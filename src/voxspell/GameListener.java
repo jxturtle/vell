9ddd0206a0de23857d7ba2525c766aa872475879
jxturtle <jxia616@aucklanduni.ac.nz>
2016-09-21 19:08:14 +1200

    Delete GameListener.java

--- src/voxspell/GameListener.java
+++
@@ -1,8 +0,0 @@
-package voxspell;
-
-public interface GameListener {
-	void updateProgressBar(GameEvent event);
-	String getWord();
-	void setWord(GameEvent event, String word);
-	int getLength();
-}
