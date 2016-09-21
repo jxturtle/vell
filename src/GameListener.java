1357a9b63e109946cd092cfebe3bc3be44ca694a
bchj513 <bcha099@aucklanduni.ac.nz>
2016-09-21 21:45:55 +1200

    Update GameListener.java

--- src/GameListener.java
+++ src/GameListener.java
@@ -1,6 +1,16 @@
 package voxspell;
 
+/**
+ * Interface for listener for GameEvent. Adapters implement this interface to must
+ * implement the given four methods 
+ * @author CJ Bang
+ *
+ */
 public interface GameListener {
+	/*
+	 * signature methods that need implementing by the class that implements 
+	 * this interface
+	 */ 
 	void updateProgressBar(GameEvent event);
 	String getWord();
 	void setWord(GameEvent event, String word);
