d0204c59f2994c48a783ff06ece98ef6a3eece9c
bchj513 <bcha099@aucklanduni.ac.nz>
2016-09-22 20:04:02 +1200

    Update StatsModel.java

--- src/StatsModel.java
+++ src/StatsModel.java
@@ -8,7 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 /**
- * Class that draws progress bar for each level of game session. The colour
+ * Class that draws a progress bar for each level of game session. The colour
  * of the bar also reflects how well the user is playing the game session.
  * An extra representation of accuracy rate is also present on the right.
  * @author CJ Bang
@@ -55,11 +55,9 @@ public class StatsModel extends JPanel {
 	 */
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		/* 
-		 * if review mode, total words can be any value between 1 to 10 unlike
-		 * the game mode, therefore word number needs to be shown on tha panel. 
-		 * Also the percentage is shown in the same manner as in game mode.
-		 */
+		// if review mode, total words can be any value between 1 to 10 unlike
+		// the game mode, therefore word number needs to be shown on the panel. 
+		// Also the percentage is shown in the same manner as in game mode.
 		if (_level == 0) {
 			if (_totalWords > 10) {
 				_totalWords = 10;
@@ -72,17 +70,15 @@ public class StatsModel extends JPanel {
 			g.drawString("Word " + Integer.toString(wordNumber) + " of " + Integer.toString(_totalWords), 140,100);
 			Graphics2D g2 = (Graphics2D)g.create();
 			g2.setFont(new Font("Verdana", Font.BOLD, 50));
-			g2.drawString(Integer.toString(100*_wordsCorrect/_totalWords)+"%", 170,250);
-			/* 
-			 * if game mode, there are three aspects to be updated 
-			 *  1) the length of the progress bar: whenever user gets the word correct, 
-			 *     bar length increases. The maximum length is 9.
-			 *  2) the color of the progress bar: if user gets one word wrong, the color 
-			 *     is changed to yellow to give the user a warning. And to red if user 
-			 *     gets two words wrong - the color persists through the session.
-			 *  3) the accuracy rate in percentage. percentage out of 9 words and
-			 *     once user gets one word wrong, % out of 10 words is displayed. 
-			 */
+			g2.drawString(Integer.toString(100*_wordsCorrect/_totalWords)+"%", 170,250); 
+			// if game mode, there are three aspects to be updated 
+			//  1) the length of the progress bar: whenever user gets the word correct, 
+			//     bar length increases. The maximum length is 9.
+			//  2) the color of the progress bar: if user gets one word wrong, the color 
+			//     is changed to yellow to give the user a warning. And to red if user 
+			//     gets two words wrong - the color persists through the session.
+			//  3) the accuracy rate in percentage. percentage out of 9 words and
+			//     once user gets one word wrong, % out of 10 words is displayed. 
 		} else {
 			if (_wordsIncorrect >= 2) {
 				_color = Color.RED;
